8799ca82e3c5c1ac35df37c0a960a2b83797ba9c
Reandore <leandre.b.boislard@gmail.com>
2023-02-27 13:34:57 -0500

    implementation du dialog

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let video = document.querySelectorAll(".page");
+let closeBtn = document.querySelector(".close");
 
+closeBtn.addEventListener("click", () => {
+  const dialog = document.querySelector("dialog");
+  dialog.close()
+});
 
 video.forEach((e) => {
-  console.log(e);
   e.addEventListener("mouseover", () => {
     let video = e.querySelector("video");
     if (video) {
@@ -16,16 +20,33 @@ video.forEach((e) => {
     }
   });
 
-e.addEventListener('click',()=>{
+  e.addEventListener("click", () => {
+    // console.log(e.firstElementChild);
 
-  // console.log(e.firstElementChild);
+    // e.firstElementChild.requestFullscreen();
 
-  // e.firstElementChild.requestFullscreen();
-  
-  if (e.classList.contains('open')) {
-    e.classList.remove('open')    
-  }
-  else(e.classList.add('open'))
-})
+    // if (e.classList.contains('open')) {
+    //   e.classList.remove('open')
+    // }
+    // else(e.classList.add('open'))
 
+    const dialog = document.querySelector("dialog");
+
+    console.log("element", e);
+
+    console.log("parent", e.parentElement.parentElement.firstElementChild);
+
+    const title = dialog.querySelector(".title");
+
+    title.textContent =
+      e.parentElement.parentElement.firstElementChild.textContent;
+
+    const video = dialog.querySelector("video");
+
+    video.src = e.firstElementChild.src;
+
+    console.log(e.firstElementChild.src);
+
+    dialog.showModal();
+  });
 });
